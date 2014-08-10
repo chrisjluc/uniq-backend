f44ec2980f7d49428fcfbd21c34361744e3a897d
Christopher Luc <chris.luc.dev@gmail.com>
2014-08-10 00:10:45 -0400

    Added school tests

--- uniq_mongo/uniq/schools/tests.py
+++ uniq_mongo/uniq/schools/tests.py
@@ -1,13 +1,34 @@
 from rest_framework import status
-from rest_framework.test import APITestCase
+from uniq.testing.testcases import MongoTestCase
+from .models import *
 
-class SchoolTests(APITestCase):
-    def setUp(self):
-        pass
+class SchoolTests(MongoTestCase):
 
-    def tearDown(self):
-        pass
+	def setUp(self):
+		s = School(slug="s")
+		s.save()
 
-    def schools_get(self):
-        factory = APIRequestFactory()
-        request = factory.get('/schools/')
+	def tearDown(self):
+		pass
+
+	def test_get_listList(self):
+		response = self.client.get('/schools/', format='json')
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+	def test_get_detail_slug(self):
+		response = self.client.get('/schools/s/', format='json')
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+	def test_get_detail_id(self):
+		s = School(slug="f")
+		s.save()
+		response = self.client.get('/schools/%s/' % s.id, format='json')
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+	def test_get_detail_invalid_slug(self):
+		response = self.client.get('/schools/asdf/', format='json')
+		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+	def test_get_detail_invalid_id(self):
+		response = self.client.get('/schools/invalidid1/', format='json')
+		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
