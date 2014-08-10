3660fc225ef8e98b69572b6279b68022fef28b64
Christopher Luc <chris.luc.dev@gmail.com>
2014-08-10 00:09:41 -0400

    Initialize testing framework

---
+++ uniq_mongo/uniq/uniq/testing/testrunners.py
@@ -0,0 +1,34 @@
+from django.test.simple import DjangoTestSuiteRunner
+from mongoengine.python_support import PY3
+from mongoengine import connect
+from django.conf import settings
+
+class MongoTestRunner(DjangoTestSuiteRunner):
+    """
+        A test runner that can be used to create, connect to, disconnect from, 
+        and destroy a mongo test database for standard django testing.
+
+        NOTE:
+            The MONGO_PORT and MONGO_DATABASE_NAME settings must exist, create them
+            if necessary.
+        
+        REFERENCE:
+            http://nubits.org/post/django-mongodb-mongoengine-testing-with-custom-test-runner/
+    """
+
+    mongodb_name = 'test_%s' % (settings.MONGO_DATABASE_NAME, )
+    
+    def setup_databases(self, **kwargs):
+        from mongoengine.connection import connect, disconnect
+        disconnect()
+        connect(self.mongodb_name, port=settings.MONGO_PORT)
+        print 'Creating mongo test database ' + self.mongodb_name
+        return super(MongoTestRunner, self).setup_databases(**kwargs)
+
+    def teardown_databases(self, old_config, **kwargs):
+        from mongoengine.connection import get_connection, disconnect
+        connection = get_connection()
+        connection.drop_database(self.mongodb_name)
+        print 'Dropping mongo test database: ' + self.mongodb_name
+        disconnect()
+        super(MongoTestRunner, self).teardown_databases(old_config, **kwargs)
