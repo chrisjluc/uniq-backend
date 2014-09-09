741caa332305263887da853915589390f597a9a4
Christopher Luc <chris.luc.dev@gmail.com>
2014-09-09 15:33:38 -0400

    Fix featured to handle query dict from django and not show expired items

--- featured/views.py
+++ featured/views.py
@@ -7,6 +7,11 @@ from rest_framework import status
 
 from django.conf import settings
 from django.core.cache import caches
+from django.http import QueryDict
+
+from bson.objectid import ObjectId
+
+import datetime
 
 cache = caches['default']
 
@@ -17,17 +22,21 @@ class FeaturedList(generics.ListCreateAPIView):
 	def get_queryset(self):
 		featured = cache.get('featured')
 		if not featured:
-			featured = Featured.objects.limit(settings.MAX_FEATURED)
+			featured = Featured.objects(dateExpired__gt=datetime.datetime.now()).limit(settings.MAX_FEATURED)
 			cache.set('featured', featured)
 		return featured
 
 	def create(self, request, *args, **kwargs):
 		data = request.DATA
 		try:
+			if type(data) is QueryDict:
+				data = dict(data.iterlists())
+				for k, v in data.iteritems():
+					data[k] = v[0]
+
 			featured = Featured(**data)
 			featured.save()
 		except ValidationError, e:
 			return Response(e.to_dict(), status=status.HTTP_400_BAD_REQUEST)
-		except:
-			return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 		return Response(status=status.HTTP_201_CREATED)
